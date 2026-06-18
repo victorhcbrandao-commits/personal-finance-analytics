388cd3dd11145ca67fa0ed7b91c596615084c668
Victor Hugo <victorhcbrandao@gmail.com>
2026-06-18 13:21:04 -0300

    Implementa visualização do saldo mensal com Matplotlib

--- src/visualization.py
+++ src/visualization.py
@@ -9,15 +9,26 @@ def grafico_saldo_mensal(resumo_mensal):
         + resumo_mensal["mes"].astype(str).str.zfill(2)
     )
 
+    plt.figure(figsize=(10, 5))
+
     plt.plot(
         resumo_mensal["periodo"],
-        resumo_mensal["Saldo"]
+        resumo_mensal["Saldo"],
+        marker="o"
+    )
+
+    plt.axhline(
+        y=0,
+        color="red",
+        linestyle="--"
     )
 
     plt.title("Saldo Mensal Projetado")
     plt.xlabel("Período")
     plt.ylabel("Saldo")
-
     plt.xticks(rotation=45)
+    plt.grid()
+
+    plt.tight_layout()
 
     plt.show()
