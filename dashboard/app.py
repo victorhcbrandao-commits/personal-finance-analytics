d393b73936a71c5105b5d7acf50788a5cdb53833
Victor Hugo <victorhcbrandao@gmail.com>
2026-06-19 12:03:44 -0300

    Implementa fluxo de caixa mensal profissional

--- dashboard/app.py
+++ dashboard/app.py
@@ -1,5 +1,9 @@
 import streamlit as st
 import plotly.express as px
+import plotly.graph_objects as go
+
+
+from io import BytesIO
 
 from src.load_data import carregar_transacoes
 from src.clean_data import limpar_transacoes
@@ -19,11 +23,34 @@ st.title(
     "FinSight - Personal Finance Analytics"
 )
 
+st.caption(
+    "Panorama completo da sua vida financeira"
+)
+
 df = carregar_transacoes("data/raw/transacoes.csv")
 
 df = limpar_transacoes(df)
 
 st.sidebar.title(
+    "FinSight"
+)
+
+pagina = st.sidebar.radio(
+    "Menu",
+    [
+        "🏠 Visão Geral",
+        "📈 Fluxo de Caixa",
+        "💳 Cartões de Crédito",
+        "📅 Planejamento",
+        "🎯 Metas",
+        "📊 Relatórios",
+        "⚙️ Configurações"
+    ]
+)
+
+st.sidebar.divider()
+
+st.sidebar.subheader(
     "Filtros"
 )
 
@@ -109,7 +136,37 @@ if tipo_pagamento_selecionado != "Todos":
 
 top_despesas = maiores_despesas(df)
 
+top_despesas["valor_formatado"] = (
+    top_despesas["valor"]
+    .apply(
+        lambda x:
+        f"R$ {x:,.0f}"
+        .replace(",", ".")
+        if x.is_integer()
+        else
+        f"R$ {x:,.2f}"
+        .replace(",", "X")
+        .replace(".", ",")
+        .replace("X", ".")
+    )
+)
+
 top_receitas = maiores_receitas(df)
+
+top_receitas["valor_formatado"] = (
+    top_receitas["valor"]
+    .apply(
+        lambda x:
+        f"R$ {x:,.0f}"
+        .replace(",", ".")
+        if x.is_integer()
+        else
+        f"R$ {x:,.2f}"
+        .replace(",", "X")
+        .replace(".", ",")
+        .replace("X", ".")
+    )
+)
 
 categorias = gastos_por_categoria(df)
 
@@ -131,47 +188,231 @@ total_despesas = df_resumo_mensal["Despesa"].sum()
 
 saldo_acumulado = df_resumo_mensal["Saldo"].sum()
 
-col1, col2, col3 = st.columns(3)
+quantidade_transacoes= len(df)
 
-col1.metric(
-    "Receitas",
-    f"R$ {total_receitas:.2f}"
+def formatar_moeda(valor):
+
+    return (
+        f"R$ {valor:,.2f}"
+        .replace(",", "X")
+        .replace(".", ",")
+        .replace("X", ".")
+    )
+
+def card_kpi(titulo, valor, icone, cor):
+
+    st.markdown(
+        f"""
+<div style='background-color:#111827; padding:20px; border-radius:16px; border:1px solid #1f2937; min-height:120px;'>
+    <div style='display:flex; justify-content:space-between; align-items:center;'>
+        <span style='font-size:16px; color:#cbd5e1;'>{titulo}</span>
+        <span style='font-size:20px;'>{icone}</span>
+    </div>
+    <div style='font-size:24px; font-weight:700; color:{cor}; margin-top:18px; white-space:nowrap;'>
+        {valor}
+    </div>
+</div>
+        """,
+        unsafe_allow_html=True
+    )
+
+col1, col2, col3, col4 = st.columns(4)
+
+with col1:
+    card_kpi("Receitas", formatar_moeda(total_receitas), "💰", "#00CC96")
+
+with col2:
+    card_kpi("Despesas", formatar_moeda(total_despesas), "📉", "#EF553B")
+
+with col3:
+    card_kpi("Saldo", formatar_moeda(saldo_acumulado), "💳", "#636EFA")
+
+with col4:
+    card_kpi("Transações", quantidade_transacoes, "📊", "#FFFFFF")
+
+st.subheader(
+    "Saldo Mensal Projetado"
 )
 
-col2.metric(
-    "Despesas",
-    f"R$ {total_despesas:.2f}"
-)
-
-col3.metric(
-    "Saldo",
-    f"R$ {saldo_acumulado:.2f}"
-)
-
-st.subheader("Saldo Mensal Projetado")
-
-st.line_chart(
+fig = px.line(
     df_resumo_mensal,
     x="periodo",
-    y="Saldo"
+    y="Saldo",
+    markers=True
+)
+
+fig.update_layout(
+    xaxis_title="Período",
+    yaxis_title="Valor",
+    legend_title="Tipo"
+)
+
+st.plotly_chart(
+    fig,
+    use_container_width=True
+)
+
+df_grafico = df_resumo_mensal.copy()
+
+df_grafico["Saldo Acumulado"] = (
+    df_grafico["Saldo"].cumsum()
+)
+
+
+fig = go.Figure()
+
+fig.add_trace(
+    go.Bar(
+        x=df_grafico["periodo"],
+        y=df_grafico["Receita"],
+        name="Receitas",
+        marker_color="#00CC96"
+    )
+)
+
+fig.add_trace(
+    go.Bar(
+        x=df_grafico["periodo"],
+        y=df_grafico["Despesa"],
+        name="Despesas",
+        marker_color="#EF553B"
+    )
+)
+
+fig.add_trace(
+    go.Scatter(
+        x=df_grafico["periodo"],
+        y=df_grafico["Saldo Acumulado"],
+        mode="lines+markers",
+        name="Saldo Acumulado",
+        line=dict(
+            color="#636EFA",
+            width=3
+        ),
+        marker=dict(size=8)
+    )
+)
+
+fig.update_layout(
+
+    barmode="group",
+
+    xaxis_title="",
+
+    yaxis_title="",
+
+    template="plotly_dark",
+
+    height=500,
+
+    hovermode="x unified",
+
+    legend=dict(
+        orientation="h",
+        x=0,
+        y=1.10
+    )
+)
+
+st.subheader(
+    "Fluxo de Caixa Mensal"
+)
+
+st.plotly_chart(
+    fig,
+    use_container_width=True
 )
 
 st.subheader(
     "Top Maiores Despesas"
 )
 
-st.dataframe(
+
+fig = px.bar(
     top_despesas,
-    hide_index=True
+    x="valor",
+    y="descricao",
+    orientation="h",
+    text="valor"
+)
+
+fig.update_layout(
+    yaxis={
+        "categoryorder": "total ascending"
+    }
+)
+
+fig.update_traces(
+    text=top_despesas["valor_formatado"],
+    textposition="outside"
+)
+
+fig.update_layout(
+    xaxis_title="Valor (R$)",
+    yaxis_title="",
+    showlegend=False,
+    yaxis={
+        "categoryorder": "total ascending"
+    }
+)
+
+fig.update_xaxes(
+    range=[0, top_despesas["valor"].max() * 1.25]
+)
+
+fig.update_xaxes(
+    visible=False
+)
+
+fig.update_layout(
+    xaxis_title="Valor (R$)",
+    yaxis_title="",
+    showlegend=False
+)
+
+fig.update_layout(
+    xaxis_title=""
+)
+
+st.plotly_chart(
+    fig,
+    use_container_width=True
 )
 
 st.subheader(
     "Top Receitas"
 )
 
-st.dataframe(
+fig = px.bar(
     top_receitas,
-    hide_index=True
+    x="valor",
+    y="descricao",
+    orientation="h",
+    text="valor_formatado",
+    color_discrete_sequence=["#00CC96"]
+)
+
+fig.update_traces(
+    textposition="outside"
+)
+
+fig.update_xaxes(
+    range=[0, top_receitas["valor"].max() * 1.25],
+    visible=False
+)
+
+fig.update_layout(
+    xaxis_title="",
+    yaxis_title="",
+    showlegend=False,
+    yaxis={
+        "categoryorder": "total ascending"
+    }
+)
+
+st.plotly_chart(
+    fig,
+    use_container_width=True
 )
 
 st.subheader(
@@ -183,8 +424,27 @@ st.dataframe(
     hide_index=True
 )
 
-st.bar_chart(
-    categorias.set_index("categoria")["valor"]
+fig = px.bar(
+    categorias,
+    x="categoria",
+    y="valor",
+    text="valor"
+)
+
+fig.update_layout(
+    xaxis_title="Categoria",
+    yaxis_title="Valor",
+    showlegend=False
+)
+
+fig.update_traces(
+    texttemplate="R$ %{text:.2f}",
+    textposition="outside"
+)
+
+st.plotly_chart(
+    fig,
+    use_container_width=True
 )
 
 st.subheader(
@@ -287,6 +547,28 @@ df_exibicao["Valor"] = (
         .replace(".", ",")
         .replace("X", ".")
     )
+)
+
+buffer = BytesIO()
+
+df_exibicao.to_excel(
+    buffer,
+    index=False,
+    engine="openpyxl"
+)
+
+buffer.seek(0)
+
+st.download_button(
+    label="📥 Baixar Transações em Excel",
+    data=buffer,
+    file_name="transacoes.xlsx",
+    mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+)
+
+excel = df_exibicao.to_excel(
+    "transacoes.xlsx",
+    index=False
 )
 
 st.subheader(
